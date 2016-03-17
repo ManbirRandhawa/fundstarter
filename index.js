4426d945ca3d5c80b487268cf18d793d63dce324
Ubuntu <ubuntu@ip-172-31-40-212.us-west-2.compute.internal>
2016-03-17 07:35:45 +0000

    part1a added port

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ var http = require("http");
 var fs = require("fs");
   
 var data = fs.readFileSync('index.html'); 
-
+var port = process.env.PORT || 8080;
 http.createServer(function(request, response){
 response.writeHeader(200, {"Content-Type": "text/html"});
 console.log("it worked");
 response.write(data);
 console.log("Loaded the Page!");
 response.end();
-}).listen(8080);
+}).listen(port);
 
 
 
