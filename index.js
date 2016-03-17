9d8cc2173be1ed5148777cf3ec986bf017d1baf5
Ubuntu <ubuntu@ip-172-31-40-212.us-west-2.compute.internal>
2016-03-17 21:39:54 +0000

    part2

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 var http = require("http");
 var fs = require("fs");
+var fileName = 'index.html';
 
 var port = process.env.PORT || 8080;
 
-fs.readFile('index.html', function (err,data)
-{
-if (err)
-{
-throw err;
-}
-
 http.createServer(function(request, response){
-response.writeHeader(200, {"Content-Type": "text/html"});
+    response.writeHeader(200, {"Content-Type": "text/html"});
 console.log("it worked");
-response.write(data);
-console.log("Loaded the Page!");
-response.end();
-}).listen(port);
+fs.exists(fileName, function(exists){
+    if (exists){
+	fs.stat(fileName, function(error, stats){
+	    fs.open(fileName, 'r', function(error,fd){
+		var buffer = new Buffer(stats.size);
+		fs.read(fd, buffer, 0 ,buffer.length,null,function(error,bytesRead, buffer){
+
+		    var data = buffer.toString('utf8', 0, buffer.length);
+		    response.write(data);
+		    console.log(data);
+		    fs.close(fd);
+		    response.end();
+
+		});
+		});
+	    });
+	}
+       
+    
+	      
 });
-
-
+	  }).listen(port);
+   
